8708a8f47f70f45b58d3ed5cd9c9fe706a49c437
Laika May <66331364+ChinamaykzaLaika@users.noreply.github.com>
2022-03-30 00:56:07 -0400

    Create ScholarshipSpecification.java

---
+++ ScholarshipSpecification.java
@@ -0,0 +1,8 @@
+interface ScholarshipReport {
+    public abstract String getName();
+    public abstract boolean isQualified(Student s);
+}
+public abstract class ScholarshipSpecification {
+
+    
+}
